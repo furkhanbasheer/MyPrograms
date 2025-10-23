db20cffeb151b63479e8a06a4525afd1ebdae585
Burkhanudeen B <furkhanbasheer001@gmail.com>
2025-10-23 20:57:40 +0530

    Add class withoutMain with a non-static block

---
+++ withoutMain.java
@@ -0,0 +1,9 @@
+public class withoutMain {
+    {
+        System.out.println("Hello World without main code!");
+    }
+
+    public static void main(String[] args) {
+    Main m = new Main();
+    }
+}
